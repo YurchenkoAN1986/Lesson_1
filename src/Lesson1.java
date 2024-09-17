64a8bcd2282f8ba4f78418febb08cf531eb8cff5
YurchenkoAN1986 <worker70@yandex.ru>
2024-09-17 17:46:12 +0300

    UPD 17-09-24

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -76,7 +76,41 @@ public static boolean isNonNegative() {
     return number1 >= 0;
 }
 
+public static void printString(String str, int count){
+    for(int i = 0; i<count; i++){
+        System.out.println(str);
+    }
+}
 
+public static boolean checkLeapYear(int year){
+    if(year % 4 == 0){
+        if(year % 100 ==0){
+            if(year % 400 == 0){
+                return true;
+            } else {
+                return false;
+            }
+        }else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+
+public static int[] swapElements() {
+    int[] arr = {0, 0, 0, 1, 1, 1, 0, 0, 0};
+
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] == 0) {
+            arr[i] = 1;
+        } else {
+            arr[i] = 0;
+        }
+    }
+
+    return arr;
+}
 
 
 public static void main(String[] args) {
@@ -87,5 +121,10 @@ public static void main(String[] args) {
     System.out.println(checkSum(10, 10));
     checkNumbers();
     System.out.println("Результат: " + isNonNegative());
+    printString("Hello World", 3);
+    System.out.println("2024 is a leap year: " + checkLeapYear(2024));
+    for (int num : swapElements()) {
+        System.out.print(num + " ");
+    }
 }
 }
